a59bfc8be46f3aac6619158310d95b60eeded776
Tsora1216 <so12ra16@icloud.com>
2022-09-09 21:29:53 +0900

    日付選択で、リストを動的表示を可能にした

--- lib/calender_screen.dart
+++ lib/calender_screen.dart
@@ -11,7 +11,6 @@ class EventData {
 
 class CalenderScreen extends StatefulWidget {
   const CalenderScreen({super.key});
-
   @override
   State<CalenderScreen> createState() => _CalenderPageState();
 }
@@ -41,52 +40,83 @@ class _CalenderPageState extends State<CalenderScreen> {
 
   @override
   Widget build(BuildContext context) {
+    final screenSize = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
         title: Text("カレンダー"),
       ),
-      body: Stack(
-        children: [
-          Center(
-            child: TableCalendar(
-              selectedDayPredicate: (day){
-                return isSameDay(_selectedDay,day);
-              },
-              onDaySelected: (selectedDay, focusedDay) {
-                print(isSameDay(selectedDay,_selectedDay));
-                if (!isSameDay(selectedDay,_selectedDay))
-                setState(() {
-                  _selectedDay = selectedDay;
-                  _focusedDay = focusedDay; // update `_focusedDay` here as well
-                });
-              },
-              firstDay: DateTime.utc(2022, 4, 1),
-              lastDay: DateTime.utc(2025, 12, 31),
-              focusedDay: _focusedDay,
-              locale: 'ja_JP',
+      body: Container(
+        height: screenSize.height,
+        child: Stack(
+          children: [
+            SingleChildScrollView(
+              child: Column(
+                children: [
+                  TableCalendar(
+                    selectedDayPredicate: (day) {
+                      return isSameDay(_selectedDay, day);
+                    },
+                    onDaySelected: (selectedDay, focusedDay) {
+                      final filterEvent = eventList
+                          .where((element) =>
+                              isSameDay(element.date, selectedDay))
+                          .toList();
+                      if (filterEvent.isNotEmpty) {
+                        print(filterEvent[0].events);
+                      }
+                      print(isSameDay(selectedDay, _selectedDay));
+                      if (!isSameDay(selectedDay, _selectedDay))
+                        setState(() {
+                          _selectedDay = selectedDay;
+                          _focusedDay =
+                              focusedDay; // update `_focusedDay` here as well
+                          if (filterEvent.isNotEmpty) {
+                            selectEvent = filterEvent.first.events;
+                          }
+                        });
+                    },
+                    firstDay: DateTime.utc(2022, 4, 1),
+                    lastDay: DateTime.utc(2025, 12, 31),
+                    focusedDay: _focusedDay,
+                    locale: 'ja_JP',
+                  ),
+                  ListView(
+                    shrinkWrap: true,
+                    children: [
+                      for (var event in selectEvent)
+                        ListTile(
+                          title: Text(event),
+                        )
+                    ],
+                  ),
+                ],
+              ),
             ),
-          ),
-          Positioned(
-            right: 20,
-            bottom: 20,
-            child: Container(
-              height: 50,
-              color: Colors.red,
-              width: 50,
+            Positioned(
+              bottom: 24,
+              right: 24,
               child: GestureDetector(
-                onTap: () => null,
-                child: Icon(
-                  Icons.arrow_forward,
-                  color: Colors.white,
-                  size: 30.0,
+                onTap: ()=> print("A"),
+                child: Container(
+                  height: 50,
+                  color: Colors.red,
+                  width: 50,
+                  child: GestureDetector(
+                    onTap: () => null,
+                    child: Icon(
+                      Icons.arrow_forward,
+                      color: Colors.white,
+                      size: 30.0,
+                    ),
+                  ),
                 ),
               ),
             ),
-          )
-        ],
+          ],
+        ),
       ),
     );
   }
-  void _selectRightButton(){
-  }
+
+  void _selectRightButton() {}
 }
